7772a8312cb386e9b509c147de6f6fd138a1ebf8
aakash <spysky1998@gmail.com>
2021-05-30 21:30:19 -0700

    aaka

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const { isEmpty } = require('lodash')
-const mongoose = require('mongoose')
-require('./connection')
-const app = express()
-const port = process.env.PORT || 5000
-app.use(express.json())
+const express = require('express');
+const { isEmpty } = require('lodash');
+const mongoose = require('mongoose');
+require('./connection');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json());
 
 
 const schema = new mongoose.Schema({
@@ -17,25 +17,25 @@ const schema = new mongoose.Schema({
  
 });
 
-// app.post('/update/:mId', async(req, res)=>{
-//     const check =await getDataOfSpecificMachine(req.params.mId).find();
-//     const filter = {_id : check[0]._id}
-//     const change = {product_one_status:faturelse}
-//     const update = await getDataOfSpecificMachine(req.params.mId).findOneAndUpdate(filter, change);
-//     res.send(update)
-//     console.log(update)
+app.post('/update/:mId', async(req, res)=>{
+    const left = req.body;
+    const check =await getDataOfSpecificMachine(req.params.mId).find();
+    const filter = {_id : left["_id"]};
+    const change = {quantity:left["quantity"]};
+    const update = await getDataOfSpecificMachine(req.params.mId).findOneAndUpdate(filter, change);
+    return res.status(200).send(update);
 
-// })
+});
 
 
 app.post('/fillProduct/:mId', async(req, res) => {
     try{
         // here we check the machine is registered or not. if not registered then we will not proceed.
-        const machineId = req.params.mId
+        const machineId = req.params.mId;
         const validate = await getDataOfSpecificMachine(machineId).find();
-        const isNotValid = Object.keys(validate).length === 0
+        const isNotValid = Object.keys(validate).length === 0;
         if(isNotValid){
-            res.status(400).send("This machine is not valid. Please contact to our team for validation")
+            return res.status(400).send("This machine is not valid. Please contact to our team for validation");
         }
         else{
             const fillProduct = new getDataOfSpecificMachine(machineId)({
@@ -47,40 +47,37 @@ app.post('/fillProduct/:mId', async(req, res) => {
                 status:req.body.status,
 
             })
-            const fill = await fillProduct.save()
-            res.status(201).send(fill)
-            console.log(fill)
-        }        
+            const fill = await fillProduct.save();
+            return res.status(201).send(fill);
+        };        
 
     }catch(e){
-        console.log(e)
-        res.status(404).send("Something went wrong...")
-    }
-})
+        return res.status(404).send("Something went wrong...");
+    };
+});
 
 // When we scan then This get method shows all the product discription in app.
 app.get('/showProduct/:mId', async(req, res) =>{
     try{
         const machineId = req.params.mId;
         const showAllProduct = await getDataOfSpecificMachine(machineId).find();
-        const isNotWorking = Object.keys(showAllProduct).length === 0
+        const isNotWorking = Object.keys(showAllProduct).length === 0;
         if(isNotWorking){
-            res.status(404).send("Sorry this machine is not working.")
+            return res.status(404).send("Sorry this machine is not working.");
         }
         else{
             // here we send all data
-            res.send(showAllProduct)
+            return res.status(200).send(showAllProduct);
         }
     }catch(e){
-        console.log("The problem is here - ", e)
-        res.status(404).send("Something went wrong.Please try again later.")
+        return res.status(404).send("Something went wrong.Please try again later.");
     }
 })
 
 
 function getDataOfSpecificMachine(machineId){
-    const model = new mongoose.model(`${machineId}`, schema)
-    return model
+    const model = new mongoose.model(`${machineId}`, schema);
+    return model;
 }
 
-app.listen(port, console.log(`Connection is successfully connected at ${port}`))
+app.listen(port, console.log(`Connection is successfully connected at ${port}`));
