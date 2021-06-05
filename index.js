06b04a04d08d02d4d6db3d986f1464ddb46d8a64
aakash <spysky1998@gmail.com>
2021-06-05 07:41:30 -0700

    aakash

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/updateProduct', async(req, res)=>{
         const change = {price: data["price"], quantity:data["quantity"]}; // here we add logic for adding image
         const update = await getDataOfSpecificMachine(data["machineId"]).findOneAndUpdate(filter, change);
         console.log(update)
-        return res.status(200).send("Successfully");
+        return res.status(200).send({"Successful":"yes"});
     }catch(error){
         return res.status(400).send(error);
     };
