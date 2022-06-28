9196a25ca17b101d152d3c417ba0cd22272f39b0
aakash <spysky1998@gmail.com>
2022-06-28 16:11:47 -0700

    aaassssss

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const schema = new mongoose.Schema({
     price:Number,
     status:Boolean,
 	product_id : Number, 
+    image:String,
     machine:String,
     key_razorpay:String
 });
@@ -175,15 +176,15 @@ app.get('/getQuantity', async(req, res)=>{
     };
 });
 
+
 // When we scan then This get method shows all the product discription in app.
 app.get('/showProduct/:mId', async(req, res) =>{
     try{
         const machineId = req.params.mId;
-        const showAllProduct = await getDataOfSpecificMachine(machineId).find({machine:machineId});
+        const showAllProduct = await getDataOfSpecificMachine(machineId).find({});
         const isNotWorking = Object.keys(showAllProduct).length === 0;
         if(isNotWorking){
-            console.log(showAllProduct);
-            return res.status(404).send(showAllProduct);
+            return res.status(404).send("Sorry this machine is not working.");
         }
         else{
             // here we send all data
