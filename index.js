a7e1b95dd8bab519c37432ff9e5cf118f67152ab
aakash <spysky1998@gmail.com>
2022-06-28 15:15:32 -0700

    aaas

--- index.js
+++ index.js
@@ -185,6 +185,7 @@ app.get('/showProduct/:mId', async(req, res) =>{
     try{
         const machineId = req.params.mId;
         const showAllProduct = await getDataOfSpecificMachine(machineId).find();
+        console.log(showAllProduct);
         const isNotWorking = Object.keys(showAllProduct).length === 0;
         if(isNotWorking){
             return res.status(404).send("Sorry this machine is not working.");
